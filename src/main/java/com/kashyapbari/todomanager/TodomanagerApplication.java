c7470ce8e996b7ce2b00ad6ceecfa7181fd9941f
kashyapbari <bari.r.kash@gmail.com>
2021-02-10 17:57:46 +0530

    initial spring boot config

---
+++ src/main/java/com/kashyapbari/todomanager/TodomanagerApplication.java
@@ -0,0 +1,13 @@
+package com.kashyapbari.todomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodomanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kashyapbari/todomanager/TodomanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kashyapbari.todomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodomanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
